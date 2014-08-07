68272fc9de8b19b19d3735855290e3f76ae8a95d
chanble <chanbo.cn@gamil.com>
2014-08-07 22:37:49 +0800

    init emacs config file

---
+++ emacs.el
@@ -0,0 +1,53 @@
+(setq default-tab-width 4)
+(setq tab-width 4)
+(global-set-key (kbd "RET") 'newline-and-indent)
+
+(setq inhibit-startup-message t)
+(global-linum-mode t)
+(global-hl-line-mode t)
+;;匹配括号
+(show-paren-mode t)
+(setq show-paren-style 'parentheses)
+
+(add-hook 'html-mode-hook
+        (lambda ()
+          ;; Default indentation is usually 2 spaces, changing to 4.
+          (set (make-local-variable 'sgml-basic-offset) 4)))
+
+
+(global-font-lock-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(current-language-environment "UTF-8")
+ '(font-use-system-font t)
+ '(menu-bar-mode nil)
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(text-mode-hook (quote (turn-on-auto-fill text-mode-hook-identify)))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Consolas" :foundry "microsoft" :slant normal :weight normal :height 128 :width normal)))))
+
+(global-set-key [(f8)] 'loop-alpha)  ;;注意这行中的F8 , 可以改成你想要的按键
+
+(setq alpha-list '((95 65) (100 100)))
+
+(defun loop-alpha ()
+  (interactive)
+  (let ((h (car alpha-list)))                
+    ((lambda (a ab)
+       (set-frame-parameter (selected-frame) 'alpha (list a ab))
+       (add-to-list 'default-frame-alist (cons 'alpha (list a ab)))
+       ) (car h) (car (cdr h)))
+    (setq alpha-list (cdr (append alpha-list (list h))))
+    )
+)
+;;
